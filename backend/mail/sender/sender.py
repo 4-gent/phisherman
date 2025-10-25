1548543e96a4446dc043ca0117e4f6ecebd7c450
4-gent <spicyrice09@gmail.com>
2025-10-25 12:17:37 -0700

    email and user setup

--- backend/mail/sender/sender.py
+++ backend/mail/sender/sender.py
@@ -1,1 +1,35 @@
-print("mail send")
+from mailjet_rest import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ['MAILJET_API_KEY']
+api_secret = os.environ['MAILJET_SECRET_KEY']
+
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+
+def email_send():
+    print('made it to email send')
+    data = {
+        'Messages': [
+            {
+                "From": {
+                    "Email": "marlonrburog@gmail.com",
+                    "Name": "Mailjet API"
+                },
+                "To": [
+                    {
+                        "Email": "marlon.burog@sjsu.edu",
+                        "Name": "Marlon Burog"
+                    }
+                ],
+                "Subject": "First email send - mailjet",
+                "TextPart": "Test",
+                "HTMLPart":"<h3>Testing, including link <a>https://google.com</a></h3>"
+            }
+        ]
+    }
+    result = mailjet.send.create(data=data)
+    print(result.status_code)
+    print("Email result: ", result.json())
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Login from './routes/login';
 import Register from './routes/register';
 import Trainer from './routes/trainer';
 import Quiz from './routes/quiz';
+import Admin from './routes/admin';
+import Dashboard from './routes/dashboard';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
 			<Route path="/register" element={<Register />} />
 			<Route path="/trainer" element={<Trainer />} />
 			<Route path="/quiz" element={<Quiz />} />
+			<Route path="/admin" element={<Admin />} />
+			<Route path="/dashboard" element={<Dashboard />} />
 		</Routes>
 	</Router>
   );
