b2ab2a652270366363b6202d9c3501bc7e4a0952
alan <toalan1208@gmail.com>
2025-10-25 12:59:35 -0700

    Combine hook features & background

--- frontend/src/routes/quiz.js
+++ frontend/src/routes/quiz.js
@@ -1,9 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import '../styles/quiz.css'
-import FishHook from '../styles/images/fishHook.png';
+import fisherman from '../styles/images/fishing.png'
+import fish from '../styles/images/fish1.png'
+import FishHook from '../styles/images/fishHook.png'
 
 export default function Quiz() {
-    return <CursorFollowImage />;
+    return(
+        <div className='quiz-body'>
+            <div className='quiz-topbar' />
+            {/* <div className='water-background' /> */}
+            <div className='d-flex flex-row justify-content-between'>
+                <img src={fisherman} alt="fisherman" className='fisherman-image'/>
+            </div>
+            <img src={fish} alt="fish" className='fisherman-image'/>
+            {/* backgrund image */}
+            {/* fish images */}
+            {/* score box */}
+            <CursorFollowImage/>
+        </div>
+    )
 }
 
 function CursorFollowImage() {
