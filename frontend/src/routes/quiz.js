d1f3002fdbd0835053fd5857938de9dd59b6e542
alan <toalan1208@gmail.com>
2025-10-25 11:39:36 -0700

    Quiz body template complete

--- frontend/src/routes/quiz.js
+++ frontend/src/routes/quiz.js
@@ -1,10 +1,18 @@
-import React, {useState} from 'react'
+import React from 'react'
 import '../styles/quiz.css'
+import fisherman from '../styles/images/fishing.png'
+
+//game page
 
 export default function Quiz(){
     return(
-        <div>
-
+        <div className='quiz-body'>
+            <div className='d-flex flex-row justify-content-between'>
+                <img src={fisherman} alt="fisherman" className='fisherman-image'/>
+            </div>
+            {/* backgrund image */}
+            {/* fish images */}
+            {/* score box */}
         </div>
     )
 }
